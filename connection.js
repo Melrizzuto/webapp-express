883274e1cbf231703155e544bb8fe4580c993cb1
Melrizzuto <melrizzuto4@gmail.com>
2025-01-23 14:48:47 +0100

    inserted db connection datas in .env file

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"; // importo mysql2
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ciaociaociao!',
-    database: 'db_movie',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
